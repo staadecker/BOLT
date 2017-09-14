70fa6846f28c4f4966488627f0a7412e6e18f549
SUPERETDUPER <machst@live.ca>
2017-09-13 21:20:11 -0400

    Simplify ISR Method

--- button.h
+++ button.h
@@ -5,54 +5,34 @@
 #include "const.h"
 #include "controller.h"
 
+const uint8_t BUTTON_NONE = 100;
 
 //Stores the value of the button pressed or depressed
-volatile uint8_t button_pressed = 65;
+volatile uint8_t button_pressed = BUTTON_NONE;
 
 //Called when the P_BUTTON_INTERRUPT is falling
 void button_ISR() {
 
   uint8_t button = 0;
 
-  uint8_t  val  = 0;
-  uint8_t  clk  = 0;
+  for (int i = 0; i < 8; i++) {
 
-  clk = digitalRead(P_BUTTON_CLOCK);
-  while (clk == LOW)
-  {
-    clk = digitalRead(P_BUTTON_CLOCK);
-  }
+    //Wait for clock to go high
+    while (digitalRead(P_BUTTON_CLOCK) == LOW);
 
-  for (int i = 0; i < 8; i++)
-  {
-    val = digitalRead(P_BUTTON_DATA);
-    clk = digitalRead(P_BUTTON_CLOCK);
-
-    while (clk == HIGH)
-    {
-      clk = digitalRead(P_BUTTON_CLOCK);
-    }
-
-    if (val == HIGH)
+    //Read and add value to button
+    if (digitalRead(P_BUTTON_DATA) == HIGH)
     {
       button = button + 1;
     }
 
-    if (i != 7)
-    {
+    //If not last time in loop, shift bits
+    if (i != 7) {
       button = button << 1;
     }
-    else
-    {
-      break;
-    }
 
-    clk = digitalRead(P_BUTTON_CLOCK);
-
-    while (clk == LOW)
-    {
-      clk = digitalRead(P_BUTTON_CLOCK);
-    }
+    //Wait for clock to go low
+    while (digitalRead(P_BUTTON_CLOCK) == HIGH);
   }
 
   if (button > 64) {
@@ -63,7 +43,7 @@ void button_ISR() {
 
 void button_isPressed(int buttonNumber) {
   if (button_pressed == buttonNumber) {
-    button_pressed = 65;
+    button_pressed = BUTTON_NONE ;
     return true;
   }
   return false;
