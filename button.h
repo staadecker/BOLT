6a4888c54b31429442078f2bd804f0714b9e629d
Philip-Nicolas <Philip-Nicolas@Thomass-MacBook-Pro-3.local>
2017-09-26 17:18:11 -0400

    Fix header to be the same as cpp

--- button.h
+++ button.h
@@ -2,10 +2,12 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
+#include <Arduino.h>
+
 const unsigned char BUTTON_NONE = 100;
 
-bool button_isPressed(int buttonNumber);
-void button_wait(int buttonNumber);
+bool button_isPressed(uint8_t buttonNumber);
+void button_wait(uint8_t buttonNumber);
 void button_ISR();
 
 #endif
