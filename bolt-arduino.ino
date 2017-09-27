39af5f61f47481d18e3b1a79fb0857cf0304f010
SUPERETDUPER <machst@live.ca>
2017-09-26 21:33:15 -0400

    Most basic code

--- bolt-arduino.ino
+++ bolt-arduino.ino
@@ -1,30 +1,30 @@
-const int P_LED_VCC = 3;
+//const int P_LED_VCC = 3;
 const int P_LED_DATA = 9;
 const int P_LED_CLOCK = 10;
 const int P_LED_LATCH = 11;
 
-const int LED_TWO_VOLTS = 105;
+//const int LED_TWO_VOLTS = 100;
 
-const uint8_t LED_STATE_OFF = 0;
-const uint8_t LED_STATE_ON = 1;
+//const uint8_t LED_STATE_OFF = 0;
+//const uint8_t LED_STATE_ON = 1;
 
 
-//byte val = B10101010;
+byte val = B01000000;
 
-byte states[8];
+//byte states[8];
 
 void setup() {
   Serial.begin(115200);
-  
+
   // put your setup code here, to run once:
-  pinMode(P_LED_VCC, OUTPUT);
+  //pinMode(P_LED_VCC, OUTPUT);
   pinMode(P_LED_DATA, OUTPUT);
   pinMode(P_LED_CLOCK, OUTPUT);
   pinMode(P_LED_LATCH, OUTPUT);
 
-  analogWrite(P_LED_VCC, LED_TWO_VOLTS);
+  //analogWrite(P_LED_VCC, HIGH);
 
-  states[0] = LED_STATE_ON;
+  //states[0] = LED_STATE_ON;
 
   shiftOut();
 
@@ -37,26 +37,35 @@ void shiftOut() {
 
   analogWrite(P_LED_LATCH, LOW);
 
+  shiftOut(P_LED_DATA, P_LED_CLOCK, LSBFIRST, val);
+  
+  /*analogWrite(P_LED_CLOCK, LOW);
+
   //Loop for every byte
   for (uint8_t i = 0; i < 8; i++)  {
+    Serial.println("Index : " + String(i) + ". State : OFF " + String(!!(val & (1 << i))));
+    digitalWrite(P_LED_DATA, !!(val & (1 << i)));
     //Write byte
     
     switch (states[i]) {
       case LED_STATE_OFF:
-      Serial.println("Index : " + String(i) + ". State : OFF " + String(states[i]));
+        Serial.println("Index : " + String(i) + ". State : OFF " + String(states[i]));
         analogWrite(P_LED_DATA, LOW);
         break;
       case LED_STATE_ON:
-      Serial.println("Index : " + String(i) + ". State : ON " + String(states[i]));
+        Serial.println("Index : " + String(i) + ". State : ON " + String(states[i]));
         analogWrite(P_LED_DATA, LED_TWO_VOLTS);
         break;
     }
+    
 
     //Clock
-    analogWrite(P_LED_CLOCK, LED_TWO_VOLTS);
+    analogWrite(P_LED_CLOCK, HIGH);
     analogWrite(P_LED_CLOCK, LOW);
   }
+  */
 
-  analogWrite(P_LED_LATCH, LED_TWO_VOLTS);
+  analogWrite(P_LED_LATCH, HIGH);
+  Serial.println("Done");
 }
 
