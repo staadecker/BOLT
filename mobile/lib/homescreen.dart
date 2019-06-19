e3fd828878b3fe558cd7a1db61c5028f45493df1
Martin Staadecker <machstg@gmail.com>
2019-06-19 17:08:51 -0400

    Moved body to stateful widget so Navigator can be accessed.
    Signed-off-by: Martin Staadecker <machstg@gmail.com>

--- mobile/lib/homescreen.dart
+++ mobile/lib/homescreen.dart
@@ -9,30 +9,40 @@ class BoltApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bolt',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        primaryColor: Colors.red,
-        accentColor: Colors.redAccent,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: new Text("Bolt"),
+        title: 'Bolt',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          primaryColor: Colors.red,
+          accentColor: Colors.redAccent,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(
-                child: Text("Play"),
-                onPressed: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => GamePage()));
-                },
-              ),
-            ],
+        home: Scaffold(
+            appBar: AppBar(
+              title: new Text("Bolt"),
+            ),
+            body: BoltAppBody()));
+  }
+}
+
+class BoltAppBody extends StatefulWidget {
+  @override
+  _BoltAppBodyState createState() => _BoltAppBodyState();
+}
+
+class _BoltAppBodyState extends State<BoltAppBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          RaisedButton(
+            child: Text("Play"),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => GamePage()));
+            },
           ),
-        ),
+        ],
       ),
     );
   }
